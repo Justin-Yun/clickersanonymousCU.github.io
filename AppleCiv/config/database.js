de49fc4d933588facbfbb163d25c201668179975
Thomas Quinn Langsfeld <tqlangsfeld@gmail.com>
2018-04-12 17:10:50 -0600

    added AppleCiv directory

---
+++ AppleCiv/config/database.js
@@ -0,0 +1,10 @@
+// config/database.js
+module.exports = {
+    'connection': {
+        'host': 'localhost',
+        'user': 'root',
+        'password': 'password' //need a database and password
+    },
+	'database': 'my_schema', // need a database and password
+    'users_table': 'UserInfo' 
+};
